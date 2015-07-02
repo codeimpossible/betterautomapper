1820ba4a1d3415eae1082aa2c6542a8e911beb0a
Jared Barboza <jared.m.barboza@gmail.com>
2015-07-02 13:01:47 -0400

    initial work, pretty much BAM v1 here

---
+++ BetterAutoMapper.Tests/BetterAutoMapperTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace BetterAutoMapper.Tests
+{
+    public class John
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Jared
+    {
+        public string FirstName { get; set; }
+    }
+
+
+    public class BetterAutoMapperTests
+    {
+        [Fact]
+        public void ShouldMapClasses()
+        {
+            var john = new John { FirstName = "John!", LastName = "Bubriski!" };
+
+            var jared = new BetterAutoMapper().Map<John, Jared>(john);
+
+            Assert.Equal(jared.FirstName, "John!");
+        }
+
+        [Fact]
+        public void ShouldMapClassesTheOtherWay()
+        {
+            var jared = new Jared { FirstName = "Jared!" };
+
+            var john = new BetterAutoMapper().Map<Jared, John>(jared);
+
+            Assert.Equal(john.FirstName, "Jared!");
+            Assert.Equal(john.LastName, null);
+        }
+    }
+}
---
+++ BetterAutoMapper/BetterAutoMapper.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace BetterAutoMapper
+{
+    public static class BAM
+    {
+        public static readonly BetterAutoMapper BOOM = new BetterAutoMapper();
+    }
+
+    public class BetterAutoMapper
+    {
+        public TTo Map<TFrom, TTo>(TFrom from)
+        {
+            var json = JsonConvert.SerializeObject(from);
+            return JsonConvert.DeserializeObject<TTo>(json);
+        }
+    }
+}
